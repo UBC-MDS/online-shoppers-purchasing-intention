9e585884b11548d28df90e4ded0c881dd8cc71f3
Ting Zhe Yan <tingzhe@gmail.com>
2021-12-01 22:22:51 -0800

    Add on details in data analysis md

--- reports/_build/html/_sources/data_analysis.md
+++ reports/_build/html/_sources/data_analysis.md
@@ -1,23 +1,31 @@
 # Data Analysis
 
-The data consists of numeric and categorical features. Amongst these features, "BounceRates", "ExitRates" and "PageValues" features represent the metrics measured by "Google Analytics" for each page in the e-commerce site. We have mentioned below the key observations from studying the correlation in the data:
+The data consists of numeric and categorical features. Amongst these features, "BounceRates", "ExitRates" and "PageValues" features represent the metrics measured by "Google Analytics" for each session on the e-commerce site. Below are key observations from our data analysis:
 
-- "BounceRates" and "ExitRates" are similar to each other.
-- Sessions with high BounceRates have less webpage interaction and less sales
-- "PageValues" could be an important feature. It has positive correlation with "Revenue", and is the most direct feature linking to Revenue.
+## Imbalanced Dataset
+
+![distribution_target_plot](images/chart_target_distribution.png)
+
+This dataset is imbalanced, where only 15% of sessions ended in a purchase. While 15% is not extremely imbalanced, we can still explore methods or algorithms that are better at dealing with an imbalanced dataset.
+
+## Importance of Page Values
+
+![density_plot](images/chart_density.png)
+
+`PageValues` is defined as the average page value of the pages visited by the user. In an e-commerce context, values are normally assigned to important pages such as checkout pages, or pages preceding the checkout process. As seen from the figure above, having a `PageValues` of above 5 increases the likelihood of purchase conversion. Hence, this `PageValues` feature provides a strong signal on whether the user will make a purchase or not.
 
 ## Correlation plot of the dataset
 
 ![correlation_plot](images/chart_correlation.png)
 
-We looked at the distribution of the numeric features, but this analysis proved inconclusive.
+Based on the above insight on `PageValues`, we looked at the correlation between numeric features to find whether other features are strongly correlated with `PageValues`. 
 
-## Distribution of the numerical variables
+We see that higher pageview and longer browsing duration have a positive correlation with `PageValues`. This is intuitive, as higher pageview and longer browsing duration indicate high engagement from the user. For example, if a user already had an item in mind, they might spend more time on an e-commerce website to compare prices reviews of similar products. A highly engaged user will be more likely to purchase than a passive user.
+
+Another feature of interest is `ExitRates`, which is negatively correlated with `PageValues`. `ExitRates` is defined as the average exit rate of the pages visited by the user. `ExitRates` is also positively correlated with `BounceRates`. This finding fits well in our 'engaged user' hypothesis: sessions with high `ExitRates` or `BounceRates` indicate a less engaged user, resulting in fewer purchase conversions.
+
+## Right-skewed feature distribution
+
+Finally, we also noted that many numeric features are right-skewed with long tails. This is common in e-commerce settings, whereby some users have extremely high usage statistics. We can experiment with the removal of outliers, or feature transformation such as Box-Cox, and see whether it will help in model performance.
 
 ![distribution_numerical_vars_plot](images/chart_numeric_var_distribution.png)
-
-Finally, we have highlighted below the imbalance in the taget variable. As mentioned in the data set introduction, 85% of the sessions contain a `False` class label, with the remaining 15% containing a `True` label
-
-## Target distribution
-
-![distribution_target_plot](images/chart_target_distribution.png)
--- src/eda_charts.py
+++ src/eda_charts.py
@@ -1,7 +1,7 @@
 # author: Arijeet Chatterjee
 # date: 2021-11-23
 
-'''
+"""
 TODO: Additional charts?
 
 Reads the pre-processed data and creates the charts for exploratory data analysis
@@ -11,17 +11,19 @@ Usage: src/eda_charts.py [--input_path=<input_path>] [--output_path=<output_path
 Options:
 --input_path=<input_path>          Input file path  [default: data/processed/train-eda.csv].
 --output_path=<output_path>        Folder path (exclude filename) of where to locally write the EDA charts in png format [default: results/].
-'''
+"""
 
 from docopt import docopt
 import pandas as pd
 import altair as alt
+import numpy as np
 import os
 
-alt.renderers.enable('mimetype')
-alt.data_transformers.enable('data_server')
+alt.renderers.enable("mimetype")
+alt.data_transformers.enable("data_server")
 
 opt = docopt(__doc__)
+
 
 def chart_target_distribution(data, target_var, output_path):
     """
@@ -38,17 +40,20 @@ def chart_target_distribution(data, target_var, output_path):
 
     Returns
     -------
-    
+
     """
-    ch_target_dist = alt.Chart(data).mark_bar().encode(
-        y=alt.Y(target_var, type='nominal'),
-        x=alt.X('count()', title='Count'),
-        color=alt.Color("Revenue")
-        ).properties(
-            width=450,
-            height=200
+    ch_target_dist = (
+        alt.Chart(data)
+        .mark_bar()
+        .encode(
+            y=alt.Y(target_var, type="nominal"),
+            x=alt.X("count()", title="Count"),
+            color=alt.Color("Revenue"),
         )
+        .properties(width=450, height=200)
+    )
     ch_target_dist.save(os.path.join(output_path, "chart_target_distribution.png"))
+
 
 def chart_numeric_var_distribution(data, numeric_vars, output_path):
     """
@@ -59,26 +64,29 @@ def chart_numeric_var_distribution(data, numeric_vars, output_path):
     data : pandas DataFrame
         Data frame for carrying out the EDA
     numeric_vars : list
-        List of numeric variables 
+        List of numeric variables
     output_path : str
         Path of the folder to save the chart
 
     Returns
     -------
-    
+
     """
-    ch_numeric_vars_dist = alt.Chart(data).mark_bar().encode(
-        alt.X(alt.repeat(), type='quantitative', bin=alt.Bin(maxbins=100)),
-        alt.Y('count()', title='Count'),
-        alt.Color("Revenue")
-        ).properties(
-            width=250,
-            height=200
-        ).repeat(
-            numeric_vars,
-            columns=2
+    ch_numeric_vars_dist = (
+        alt.Chart(data)
+        .mark_bar()
+        .encode(
+            alt.X(alt.repeat(), type="quantitative", bin=alt.Bin(maxbins=100)),
+            alt.Y("count()", title="Count"),
+            alt.Color("Revenue"),
         )
-    ch_numeric_vars_dist.save(os.path.join(output_path, "chart_numeric_var_distribution.png"))
+        .properties(width=250, height=200)
+        .repeat(numeric_vars, columns=2)
+    )
+    ch_numeric_vars_dist.save(
+        os.path.join(output_path, "chart_numeric_var_distribution.png")
+    )
+
 
 def chart_categorical_var_count(data, categorical_vars, output_path):
     """
@@ -89,25 +97,55 @@ def chart_categorical_var_count(data, categorical_vars, output_path):
     data : pandas DataFrame
         Data frame for carrying out the EDA
     categorical_vars : list
-        List of categorical variables 
+        List of categorical variables
     output_path : str
         Path of the folder to save the chart
 
     Returns
     -------
-    
+
     """
-    ch_categorical_vars_count = alt.Chart(data).mark_bar().encode(
-        y=alt.Y(alt.repeat(), type='nominal'),
-        x=alt.X('count()', title='Count'),
-        ).properties(
-            width=450,
-            height=200
-        ).repeat(
-            categorical_vars,
-            columns=1
+    ch_categorical_vars_count = (
+        alt.Chart(data)
+        .mark_bar()
+        .encode(
+            y=alt.Y(alt.repeat(), type="nominal"),
+            x=alt.X("count()", title="Count"),
         )
-    ch_categorical_vars_count.save(os.path.join(output_path, "chart_categorical_var_count.png"))
+        .properties(width=450, height=200)
+        .repeat(categorical_vars, columns=1)
+    )
+    ch_categorical_vars_count.save(
+        os.path.join(output_path, "chart_categorical_var_count.png")
+    )
+
+
+def density_plot(data, output_path):
+    """
+    Create and save density plot
+
+    Parameters
+    ----------
+    data : pandas DataFrame
+        Data frame for carrying out the EDA
+    output_path : str
+        Path of the folder to save the chart
+
+    Returns
+    -------
+
+    """
+    ch_density = (
+        alt.Chart(data.query("PageValues < 40"))
+        .transform_density(
+            "PageValues", groupby=["Revenue"], as_=["PageValues", "density"]
+        )
+        .mark_area(fill=None, strokeWidth=2)
+        .encode(alt.X("PageValues"), y="density:Q", stroke="Revenue")
+    )
+
+    ch_density.save(os.path.join(output_path, "chart_density.png"))
+
 
 def chart_correlation(data, output_path):
     """
@@ -122,40 +160,34 @@ def chart_correlation(data, output_path):
 
     Returns
     -------
-    
+
     """
-    corr_df = (
-        data.select_dtypes("number")
-        .corr("spearman")
-        .reset_index()
-        .melt("index")
+    corr_df = data.select_dtypes("number").corr("spearman").reset_index().melt("index")
+    corr_df.columns = ["X1", "X2", "correlation"]
+    base = (
+        alt.Chart(corr_df, title="Correlation Heatmap")
+        .transform_filter(alt.datum.X1 < alt.datum.X2)
+        .encode(
+            x=alt.X("X1", title=None),
+            y=alt.Y("X2", title=None),
         )
-    corr_df.columns = ['X1', 'X2', 'correlation']
-    base = alt.Chart(corr_df, title="Correlation Heatmap").transform_filter(
-        alt.datum.X1 < alt.datum.X2
-        ).encode(
-            x=alt.X('X1', title=None),
-            y=alt.Y('X2', title=None),
-        ).properties(
-            width=alt.Step(50),
-            height=alt.Step(50)
-        )
-    rects = base.mark_rect().encode(
-        alt.Color('correlation', scale=alt.Scale(scheme="redblue", reverse=True, domain=(-1, 1)))
+        .properties(width=alt.Step(50), height=alt.Step(50))
     )
-    text = base.mark_text(
-        size=15
-    ).encode(
-        text=alt.Text('correlation', format=".2f"),
-        color=alt.condition(
-            "datum.correlation > 0.5",
-            alt.value('white'),
-            alt.value('black')
+    rects = base.mark_rect().encode(
+        alt.Color(
+            "correlation",
+            scale=alt.Scale(scheme="redblue", reverse=True, domain=(-1, 1)),
         )
-    )    
+    )
+    text = base.mark_text(size=15).encode(
+        text=alt.Text("correlation", format=".2f"),
+        color=alt.condition(
+            "datum.correlation > 0.5", alt.value("white"), alt.value("black")
+        ),
+    )
     ch_correlation = rects + text
     ch_correlation.save(os.path.join(output_path, "chart_correlation.png"))
-    
+
 
 def main(input_path, output_path):
     """
@@ -171,22 +203,36 @@ def main(input_path, output_path):
     """
     # print(f'Entered main func')
     df = pd.read_csv(input_path)
-    numeric_cols = df.select_dtypes('number').columns.tolist()
-    category_cols = ['Month', 'VisitorType', 'Weekend']
-    target_var = 'Revenue'
-    print(f'Creating chart for distribution of the target variable')
+    target_var = "Revenue"
+    # Change target_var back to categorical for EDA
+    df[target_var] = np.where(df[target_var] == 1, "True", "False")
+
+    numeric_cols = df.select_dtypes("number").columns.tolist()
+    category_cols = ["Month", "VisitorType", "Weekend"]
+
+    print(f"Creating chart for distribution of the target variable")
     # Create chart to visualize distribution of target variable
     chart_target_distribution(data=df, target_var=target_var, output_path=output_path)
-    print(f'Creating chart for distribution of numeric variables')
+    print(f"Creating chart for distribution of numeric variables")
     # Create chart to visualize distribution of numeric variables
-    chart_numeric_var_distribution(data=df, numeric_vars=numeric_cols, output_path=output_path)
+    chart_numeric_var_distribution(
+        data=df, numeric_vars=numeric_cols, output_path=output_path
+    )
     # Create chart to visualize categorical variables
-    print(f'Creating chart for categorical variables')
-    chart_categorical_var_count(data=df, categorical_vars=category_cols, output_path=output_path)
+    print(f"Creating chart for categorical variables")
+    chart_categorical_var_count(
+        data=df, categorical_vars=category_cols, output_path=output_path
+    )
     # Create correlation plot
-    print(f'Creating correlation chart')
+    print(f"Creating correlation chart")
     chart_correlation(data=df, output_path=output_path)
-    print(f'End of EDA')
+
+    # Create Density plot
+    print(f"Creating density chart")
+    density_plot(data=df, output_path=output_path)
+
+    print(f"End of EDA")
+
 
 def test_eda_charts(input_path, output_path):
     """Test for input data type.
@@ -200,7 +246,8 @@ def test_eda_charts(input_path, output_path):
     """
 
     df = pd.read_csv(input_path)
-    assert isinstance(df, pd.DataFrame, "Error in df type")
+    assert isinstance(df, pd.DataFrame), "Error in df type"
+
 
 if __name__ == "__main__":
     test_eda_charts(opt["--input_path"], opt["--output_path"])
