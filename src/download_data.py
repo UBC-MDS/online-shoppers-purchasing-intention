1ab0d94015d5a54f486be884065cf7fc18b6fd91
Nico Van den Hooff <nicovandenhooff@gmail.com>
2021-12-03 10:50:04 -0800

    Format download data to be PEP8

--- src/download_data.py
+++ src/download_data.py
@@ -1,19 +1,21 @@
 # author: Ting Zhe Yan
 # date: 2021-11-17
 
-'''Downloads data from web to a local filepath
+"""
+Downloads data from web to a local filepath
 
 Usage: src/download_data.py [--url=<url>] [--out_path=<out_path>]
 
 Options:
 --url=<url>            URL of dataset  [default: https://archive.ics.uci.edu/ml/machine-learning-databases/00468/online_shoppers_intention.csv].
 --out_path=<out_path>  File path (including filename) of where to locally write the file [default: data/raw/online_shoppers_intention.csv].
-'''
+"""
 
 from docopt import docopt
 from urllib import request
 
 opt = docopt(__doc__)
+
 
 def main(url, out_path):
     """Main function to download dataset from the internet
@@ -24,12 +26,13 @@ def main(url, out_path):
         Direct URL to download dataset
     out_path : string
         File path (including filename) of where to locally write the file
-    """    
+    """
     try:
         request.urlretrieve(url, out_path)
     except Exception as e:
         print("Could not download file from {}".format(url))
         print(e)
-        
+
+
 if __name__ == "__main__":
     main(opt["--url"], opt["--out_path"])
